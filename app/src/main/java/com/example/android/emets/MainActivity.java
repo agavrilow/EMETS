b53ce823b695e674accb160ab9da553185f87444
AlexGee <agavrilov@ngs.ru>
2018-04-29 10:18:15 +1100

    Initial commit

---
+++ app/src/main/java/com/example/android/emets/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.emets;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //TODO:make it right
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
